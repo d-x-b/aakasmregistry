d804fef53adb934e745c127d1644d13e1e8e1225
d-x-b <15963850+d-x-b@users.noreply.github.com>
2024-10-13 20:47:59 -0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'A Kasm Registry',
+    description: 'Some Kasm workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/d-x-b/aakasmregistry',
+    contactUrl: 'https://github.com/d-x-b/aakasmregistry/support',
   },
   reactStrictMode: true,
   swcMinify: true,
